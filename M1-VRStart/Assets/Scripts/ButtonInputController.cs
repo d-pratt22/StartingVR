8d26c1f9dd6bbb95f691d8962f7c670fb357a8d4
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2026-01-31 18:38:50 -0700

    M3 DEV1

---
+++ M1-VRStart/Assets/Scripts/ButtonInputController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonInputController : MonoBehaviour
+{
+    public UnityEvent ButtonDownEvent = new UnityEvent();
+    public UnityEvent ButtonUpEvent = new UnityEvent();
+
+
+
+    void Update()
+    {
+
+    }
+}
